ea362bbd86dbd41987752f7acfef6df4017562f5
dimoslive <ntdimos@gmail.com>
2011-11-16 12:09:16 +0200

    redirect

---
+++ src/redirect.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author ntioudis
+ */
+public class redirect {
+  private String redirect;
+  
+  public redirect(String redirect_link) {
+      
+      this.redirect = redirect_link;
+      
+  }
+  
+  public String getRedirectLink() {
+      
+      return redirect;
+      
+  }
+    
+}
+
+
